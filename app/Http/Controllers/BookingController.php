c5aad3ad8e42c086012a864eb517af086377cb2c
Millen Mark Aquino <83930500+Millenmark@users.noreply.github.com>
2025-09-15 23:05:18 +0800

    feat: get request for bookings

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Booking;
 use Illuminate\Http\Request;
 
 class BookingController extends Controller
@@ -11,7 +12,8 @@ class BookingController extends Controller
      */
     public function index()
     {
-        //
+        $bookings = Booking::get();
+        return response()->json($bookings);
     }
 
     /**
