5aea228c94927d834b75d2886e46ad5193b58f0e
Millen Mark Aquino <83930500+Millenmark@users.noreply.github.com>
2025-09-18 21:50:48 +0800

    fix: make it unprotected

--- routes/api/services.php
+++ routes/api/services.php
@@ -3,6 +3,4 @@
 use App\Http\Controllers\ServiceController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
-  Route::get('/', [ServiceController::class, 'index']);
-});
+Route::get('/', [ServiceController::class, 'index']);
