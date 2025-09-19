e7e5bd80c2a79560dd4f42c41e01911341f1d93c
Millen Mark Aquino <83930500+Millenmark@users.noreply.github.com>
2025-09-19 23:47:01 +0800

    fix: route

--- routes/api/services.php
+++ routes/api/services.php
@@ -4,5 +4,5 @@ use App\Http\Controllers\ServiceController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(ServiceController::class)->group(function () {
-  Route::get('services', 'getAllServices');
+  Route::get('/', 'getAllServices');
 });
